040c08235b7f1b8a1a76923d32e665edd58c14a7
sisby-folk <sleepingdragoninn@gmail.com>
2025-06-22 14:14:47 +1000

    port to 1.20

--- src/main/java/dev/sisby/mcqoy/McQoyModMenu.java
+++ src/main/java/dev/sisby/mcqoy/McQoyModMenu.java
@@ -14,7 +14,7 @@ import java.util.Map;
 public class McQoyModMenu implements ModMenuApi {
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return null;
+		return parent -> McQoy.createScreen(parent, McQoy.ID, List.of(McQoy.CONFIG));
 	}
 
 	@Override
