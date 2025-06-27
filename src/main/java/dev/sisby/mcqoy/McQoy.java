8334fbf29540b4216a2293be6deace8542622c31
sisby-folk <sleepingdragoninn@gmail.com>
2025-06-27 22:43:02 +1000

    incompatibility placeholders

--- src/main/java/dev/sisby/mcqoy/McQoy.java
+++ src/main/java/dev/sisby/mcqoy/McQoy.java
@@ -2,6 +2,7 @@ package dev.sisby.mcqoy;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
+import dev.isxander.yacl3.api.ButtonOption;
 import dev.isxander.yacl3.api.ConfigCategory;
 import dev.isxander.yacl3.api.ListOption;
 import dev.isxander.yacl3.api.Option;
@@ -39,6 +40,8 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,6 +56,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class McQoy implements ModInitializer {
 	public static final String ID = "mcqoy";
@@ -95,7 +99,7 @@ public class McQoy implements ModInitializer {
 					Text sectionDisplayName = getDisplayName(topSection, topSection.key().getLastComponent(), NamingSchemes.TITLE_CASE);
 					category = categories.computeIfAbsent(sectionDisplayName.getString(), k -> ConfigCategory.createBuilder().name(sectionDisplayName));
 				}
-				mapAndAddField(field, category, getDisplayName(field, field.key().getLastComponent(), NamingSchemes.SPACE_SEPARATED_LOWER_CASE_INITIAL_UPPER_CASE), OptionDescription.of(getComments(field).stream().map(Text::of).toArray(Text[]::new)));
+				mapAndAddField(config, field, category, getDisplayName(field, field.key().getLastComponent(), NamingSchemes.SPACE_SEPARATED_LOWER_CASE_INITIAL_UPPER_CASE), getComments(field).stream().map(Text::of).toArray(Text[]::new));
 			}
 		}
 		for (ConfigCategory.Builder s : categories.values()) {
@@ -107,7 +111,7 @@ public class McQoy implements ModInitializer {
 	}
 
 	@SuppressWarnings({"unchecked", "rawtypes"})
-	private static void mapAndAddField(TrackedValue<?> field, ConfigCategory.Builder category, Text displayName, OptionDescription description) {
+	private static void mapAndAddField(Config config, TrackedValue<?> field, ConfigCategory.Builder category, Text displayName, Text[] description) {
 		Constraint.Range<?> tempRangeConstraint = null;
 		for (Constraint<?> constraint : field.constraints()) {
 			if (constraint instanceof Constraint.Range<?>) {
@@ -133,7 +137,10 @@ public class McQoy implements ModInitializer {
 			else if (list.getDefaultValue() instanceof Float) listOption((TrackedValue<ValueList<Float>>) field, category, displayName, description, floatOrSliderController(rangeConstraint));
 			else if (list.getDefaultValue() instanceof Double) listOption((TrackedValue<ValueList<Double>>) field, category, displayName, description, doubleOrSliderController(rangeConstraint));
 			else if (list.getDefaultValue() instanceof Enum) enumListOption(field, category, displayName, description, list, (Enum) list.getDefaultValue());
-			else LOGGER.warn("[McQoy] Unfamiliar with list field {} of class {} - skipping it!", field.key().getLastComponent(), list.getDefaultValue().getClass());
+			else {
+				LOGGER.warn("[McQoy] Unfamiliar with list field {} of class {} - displaying placeholder.", field.key().getLastComponent(), list.getDefaultValue().getClass());
+				incompatibleOption(config, category, displayName, description);
+			}
 		} else if (defaultValue instanceof ValueMapImpl<?>) {
 			ValueMapImpl<?> map = (ValueMapImpl<?>) defaultValue;
 			if (map.getDefaultValue() instanceof Boolean) mapOption((TrackedValue<ValueMap<Boolean>>) field, category, displayName, description, TickBoxControllerBuilder::create);
@@ -143,16 +150,33 @@ public class McQoy implements ModInitializer {
 			else if (map.getDefaultValue() instanceof Float) mapOption((TrackedValue<ValueMap<Float>>) field, category, displayName, description, floatOrSliderController(rangeConstraint));
 			else if (map.getDefaultValue() instanceof Double) mapOption((TrackedValue<ValueMap<Double>>) field, category, displayName, description, doubleOrSliderController(rangeConstraint));
 			else if (map.getDefaultValue() instanceof Enum) enumMapOption(field, category, displayName, description, (Enum) map.getDefaultValue());
-			else LOGGER.warn("[McQoy] Unfamiliar with map field {} of class {} - skipping it!", field.key().getLastComponent(), map.getDefaultValue().getClass());
-		} else LOGGER.warn("[McQoy] Unfamiliar with field {} of class {} - skipping it!", field.key().getLastComponent(), field.getDefaultValue().getClass());
+			else {
+				LOGGER.warn("[McQoy] Unfamiliar with map field {} of class {} - displaying placeholder.", field.key().getLastComponent(), map.getDefaultValue().getClass());
+				incompatibleOption(config, category, displayName, description);
+			}
+		} else {
+			LOGGER.warn("[McQoy] Unfamiliar with field {} of class {} - displaying placeholder.", field.key().getLastComponent(), field.getDefaultValue().getClass());
+			incompatibleOption(config, category, displayName, description);
+		}
 	}
 
-	private static <T> void singleOption(TrackedValue<T> field, ConfigCategory.Builder category, Text displayName, OptionDescription description, Function<Option<T>, ControllerBuilder<T>> controller) {
-		category.option(Option.<T>createBuilder().name(displayName).description(description).binding(field.getDefaultValue(), field::value, field::setValue).controller(controller).build());
+	private static void incompatibleOption(Config config, ConfigCategory.Builder category, Text displayName, Text[] description) {
+		Text[] desc = Stream.concat(
+			Arrays.stream(description),
+			Stream.of(
+				Text.of(""),
+				Text.of(String.format("Only editable via %s", (config.family().isEmpty() ? "" : (config.family() + "/")) + config.id() + ".toml")).copy().formatted(Formatting.YELLOW),
+				Text.of("Exit the game first.").copy().formatted(Formatting.RED)
+			)).toArray(Text[]::new);
+		category.option(ButtonOption.createBuilder().name(displayName).text(Text.of("Edit in file...")).description(OptionDescription.of(desc)).action((s, o) -> Util.getOperatingSystem().open(FabricLoader.getInstance().getConfigDir().toFile())).build());
 	}
 
-	private static <T> void listOption(TrackedValue<ValueList<T>> field, ConfigCategory.Builder category, Text displayName, OptionDescription description, Function<Option<T>, ControllerBuilder<T>> controller) {
-		category.group(ListOption.<T>createBuilder().name(displayName).description(description).binding(field.getDefaultValue(), field::value,
+	private static <T> void singleOption(TrackedValue<T> field, ConfigCategory.Builder category, Text displayName, Text[] description, Function<Option<T>, ControllerBuilder<T>> controller) {
+		category.option(Option.<T>createBuilder().name(displayName).description(OptionDescription.of(description)).binding(field.getDefaultValue(), field::value, field::setValue).controller(controller).build());
+	}
+
+	private static <T> void listOption(TrackedValue<ValueList<T>> field, ConfigCategory.Builder category, Text displayName, Text[] description, Function<Option<T>, ControllerBuilder<T>> controller) {
+		category.group(ListOption.<T>createBuilder().name(displayName).description(OptionDescription.of(description)).binding(field.getDefaultValue(), field::value,
 			l -> {
 				field.value().clear();
 				field.value().addAll(l);
@@ -160,8 +184,8 @@ public class McQoy implements ModInitializer {
 		).controller(controller).initial(field.getDefaultValue().getDefaultValue()).build());
 	}
 
-	private static <T> void mapOption(TrackedValue<ValueMap<T>> field, ConfigCategory.Builder category, Text displayName, OptionDescription description, Function<Option<T>, ControllerBuilder<T>> valueController) {
-		category.group(ListOption.<Map.Entry<String, T>>createBuilder().name(displayName).description(description).binding(
+	private static <T> void mapOption(TrackedValue<ValueMap<T>> field, ConfigCategory.Builder category, Text displayName, Text[] description, Function<Option<T>, ControllerBuilder<T>> valueController) {
+		category.group(ListOption.<Map.Entry<String, T>>createBuilder().name(displayName).description(OptionDescription.of(description)).binding(
 			field.getDefaultValue().entrySet().stream().collect(Collectors.toList()),
 			() -> field.value().entrySet().stream().collect(Collectors.toList()),
 			l -> {
@@ -172,15 +196,15 @@ public class McQoy implements ModInitializer {
 	}
 
 	@SuppressWarnings("unchecked")
-	private static <T extends Enum<T>> void enumOption(TrackedValue<?> field, ConfigCategory.Builder category, Text displayName, OptionDescription description, T defaultValue) {
-		category.option(Option.<T>createBuilder().name(displayName).description(description).binding(defaultValue, () -> (T) field.value(), v -> ((TrackedValue<T>) field).setValue(v)).controller(
+	private static <T extends Enum<T>> void enumOption(TrackedValue<?> field, ConfigCategory.Builder category, Text displayName, Text[] description, T defaultValue) {
+		category.option(Option.<T>createBuilder().name(displayName).description(OptionDescription.of(description)).binding(defaultValue, () -> (T) field.value(), v -> ((TrackedValue<T>) field).setValue(v)).controller(
 			o -> EnumControllerBuilder.create(o).enumClass(defaultValue.getDeclaringClass())
 		).build());
 	}
 
 	@SuppressWarnings("unchecked")
-	private static <T extends Enum<T>> void enumListOption(TrackedValue<?> field, ConfigCategory.Builder category, Text displayName, OptionDescription description, ValueList<?> defaultList, T defaultValue) {
-		category.group(ListOption.<T>createBuilder().name(displayName).description(description).binding((List<T>) defaultList, () -> (ValueList<T>) field.value(),
+	private static <T extends Enum<T>> void enumListOption(TrackedValue<?> field, ConfigCategory.Builder category, Text displayName, Text[] description, ValueList<?> defaultList, T defaultValue) {
+		category.group(ListOption.<T>createBuilder().name(displayName).description(OptionDescription.of(description)).binding((List<T>) defaultList, () -> (ValueList<T>) field.value(),
 			l -> {
 				((TrackedValue<ValueList<T>>) field).value().clear();
 				((TrackedValue<ValueList<T>>) field).value().addAll(l);
@@ -189,8 +213,8 @@ public class McQoy implements ModInitializer {
 	}
 
 	@SuppressWarnings("unchecked")
-	private static <T extends Enum<T>> void enumMapOption(TrackedValue<?> field, ConfigCategory.Builder category, Text displayName, OptionDescription description, T defaultValue) {
-		category.group(ListOption.<Map.Entry<String, T>>createBuilder().name(displayName).description(description).binding(
+	private static <T extends Enum<T>> void enumMapOption(TrackedValue<?> field, ConfigCategory.Builder category, Text displayName, Text[] description, T defaultValue) {
+		category.group(ListOption.<Map.Entry<String, T>>createBuilder().name(displayName).description(OptionDescription.of(description)).binding(
 			((TrackedValue<ValueMap<T>>) field).getDefaultValue().entrySet().stream().collect(Collectors.toList()),
 			() -> ((TrackedValue<ValueMap<T>>) field).value().entrySet().stream().collect(Collectors.toList()),
 			l -> {
