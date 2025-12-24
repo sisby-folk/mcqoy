486b7fcc302108c93c2d174c49f8b4b144b26bc5
sisby-folk <sleepingdragoninn@gmail.com>
2025-12-24 16:15:11 +1100

    color and matches support

--- src/main/java/dev/sisby/mcqoy/McQoy.java
+++ src/main/java/dev/sisby/mcqoy/McQoy.java
@@ -12,26 +12,26 @@ import folk.sisby.kaleido.lib.quiltconfig.api.metadata.MetadataContainer;
 import folk.sisby.kaleido.lib.quiltconfig.api.metadata.NamingScheme;
 import folk.sisby.kaleido.lib.quiltconfig.api.metadata.NamingSchemes;
 import folk.sisby.kaleido.lib.quiltconfig.api.values.TrackedValue;
+import folk.sisby.kaleido.lib.quiltconfig.api.values.ValueList;
 import folk.sisby.kaleido.lib.quiltconfig.api.values.ValueTreeNode;
 import folk.sisby.kaleido.lib.quiltconfig.impl.util.ConfigsImpl;
 import folk.sisby.kaleido.lib.quiltconfig.impl.values.ValueListImpl;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import me.shedaniel.clothconfig2.gui.entries.TextListEntry;
 import me.shedaniel.clothconfig2.impl.builders.AbstractFieldBuilder;
-import me.shedaniel.clothconfig2.impl.builders.EnumSelectorBuilder;
+import me.shedaniel.clothconfig2.impl.builders.ColorFieldBuilder;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Text;
-import net.minecraft.util.Util;
+import net.minecraft.text.TextColor;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.awt.*;
-import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -41,10 +41,10 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class McQoy implements ModInitializer {
@@ -144,6 +144,7 @@ public class McQoy implements ModInitializer {
 		final Constraint.Range<?> rangeConstraint = tempRangeConstraint;
 		Object defaultValue = field.getDefaultValue();
 		if (defaultValue instanceof Boolean) category.addEntry(option((TrackedValue<Boolean>) field, displayName, description, builder::startBooleanToggle).build());
+		else if (defaultValue instanceof String && color) category.addEntry(option((TrackedValue<String>) field, displayName, description, (BiFunction<Text, Integer, ColorFieldBuilder>) builder::startAlphaColorField, s -> colorOrWhite(s, alpha), i -> colorToString(i, alpha)).build());
 		else if (defaultValue instanceof String) category.addEntry(option((TrackedValue<String>) field, displayName, description, builder::startStrField).build());
 		else if (defaultValue instanceof Integer) slider(category, (TrackedValue<Integer>) field, displayName, description, builder::startIntField, builder::startIntSlider, rangeConstraint);
 		else if (defaultValue instanceof Long) slider(category, (TrackedValue<Long>) field, displayName, description, builder::startLongField, builder::startLongSlider, rangeConstraint);
@@ -152,54 +153,66 @@ public class McQoy implements ModInitializer {
 		else if (defaultValue instanceof Enum) category.addEntry(option((TrackedValue<Enum>) field, displayName, description, (d, e) -> builder.startEnumSelector(d, e.getDeclaringClass(), e)).build());
 		else if (defaultValue instanceof ValueListImpl<?>) {
 			ValueListImpl<?> list = (ValueListImpl<?>) defaultValue;
-			if (list.getDefaultValue() instanceof String) category.addEntry(option((TrackedValue<List<String>>) field, displayName, description, builder::startStrList).build());
-			else if (list.getDefaultValue() instanceof Integer) category.addEntry(option((TrackedValue<List<Integer>>) field, displayName, description, builder::startIntList).build());
-			else if (list.getDefaultValue() instanceof Long) category.addEntry(option((TrackedValue<List<Long>>) field, displayName, description, builder::startLongList).build());
-			else if (list.getDefaultValue() instanceof Float) category.addEntry(option((TrackedValue<List<Float>>) field, displayName, description, builder::startFloatList).build());
-			else if (list.getDefaultValue() instanceof Double) category.addEntry(option((TrackedValue<List<Double>>) field, displayName, description, builder::startDoubleList).build());
+			if (list.getDefaultValue() instanceof String) category.addEntry(listOption((TrackedValue<ValueList<String>>) field, displayName, description, builder::startStrList).build());
+			else if (list.getDefaultValue() instanceof Integer) category.addEntry(listOption((TrackedValue<ValueList<Integer>>) field, displayName, description, builder::startIntList).build());
+			else if (list.getDefaultValue() instanceof Long) category.addEntry(listOption((TrackedValue<ValueList<Long>>) field, displayName, description, builder::startLongList).build());
+			else if (list.getDefaultValue() instanceof Float) category.addEntry(listOption((TrackedValue<ValueList<Float>>) field, displayName, description, builder::startFloatList).build());
+			else if (list.getDefaultValue() instanceof Double) category.addEntry(listOption((TrackedValue<ValueList<Double>>) field, displayName, description, builder::startDoubleList).build());
 			else { // Boolean List, Enum List
 				LOGGER.warn("[McQoy] Unfamiliar with list field {} of class {} - displaying placeholder.", field.key().getLastComponent(), list.getDefaultValue().getClass());
-				incompatibleOption(config, category, field, displayName, description);
+				incompatibleOption(config, category, field, displayName, description, builder);
 			}
 		} else { // Maps
 			LOGGER.warn("[McQoy] Unfamiliar with field {} of class {} - displaying placeholder.", field.key().getLastComponent(), field.getDefaultValue().getClass());
-			incompatibleOption(config, category, field, displayName, description);
+			incompatibleOption(config, category, field, displayName, description, builder);
 		}
 	}
 
-	private static Color colorOrWhite(String string, boolean alpha) {
+	private static Integer colorOrWhite(String string, boolean alpha) {
 		try {
-			return new Color(Integer.parseUnsignedInt(string.replace("#", ""), 16), alpha);
+			return Integer.parseUnsignedInt(string.replace("#", ""), 16) | (alpha ? 0 : 0xFF_000000);
 		} catch (NumberFormatException e) {
-			return Color.WHITE;
+			return alpha ? 0xFF_FFFFFF : 0xFFFFFF;
 		}
 	}
 
-	private static String colorToString(Color color, boolean alpha) {
-		return "#" + StringUtils.leftPad(Integer.toHexString(color.getRGB() & (alpha ? 0xFF_FFFFFF : 0x00_FFFFFF)), alpha ? 8 : 6, "0");
+	private static String colorToString(Integer color, boolean alpha) {
+		return "#" + StringUtils.leftPad(Integer.toHexString(color & (alpha ? 0xFF_FFFFFF : 0x00_FFFFFF)), alpha ? 8 : 6, "0");
 	}
 
-	private static void incompatibleOption(Config config, ConfigCategory category, TrackedValue<?> field, Text displayName, Text[] description) {
+	private static void incompatibleOption(Config config, ConfigCategory category, TrackedValue<?> field, Text displayName, Text[] description, ConfigEntryBuilder builder) {
 		Text fileHint = Text.of(String.format("Only editable via %s", (config.family().isEmpty() ? "" : (config.family() + "/")) + config.id() + ".toml"));
 		Text exitHint = Text.of("Exit the game first.");
-		Text[] desc = Stream.concat(Arrays.stream(description), Stream.of(Text.of(""), fileHint, exitHint)).toArray(Text[]::new);
+		Text desc = Text.of(Stream.concat(Arrays.stream(description), Stream.of(Text.of(""), fileHint, exitHint)).map(Text::getString).collect(Collectors.joining("\n")));
 		Text label = Text.of("[Edit in file...] " + displayName);
-		// Cloth doesn't have click actions...
-		category.addEntry(new TextListEntry(Text.of(""), label, 0xFFFFFF55, () -> Optional.of(desc)) {
-			@Override
-			public boolean mouseClicked(double mouseX, double mouseY, int button) {
-				Util.getOperatingSystem().open(FabricLoader.getInstance().getConfigDir().toFile());
-				return true;
-			}
-		});
+		category.addEntry(builder.startTextDescription(label.copy().styled(s -> s
+			.withColor(TextColor.fromRgb(0xFF_FFFF55))
+			.withHoverEvent(desc.getString().isEmpty() ? null : new HoverEvent(HoverEvent.Action.SHOW_TEXT, desc))
+			.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, FabricLoader.getInstance().getConfigDir().toString()))
+		)).build());
 	}
 
 	private static <T, C, B extends AbstractFieldBuilder<C, ?, B>> B option(TrackedValue<T> field, Text displayName, Text[] description, BiFunction<Text, C, B> constructor, Function<T, C> getBinder, Function<C, T> setBinder) {
-		return constructor.apply(displayName, getBinder.apply(field.value())).setTooltip(description).setSaveConsumer(v -> field.setValue(setBinder.apply(v)));
+		return constructor.apply(displayName, getBinder.apply(field.value())).setTooltip(description).setSaveConsumer(v -> {
+			if (field.checkForFailingConstraints(setBinder.apply(v)).isPresent()) return;
+			field.setValue(setBinder.apply(v));
+		});
+	}
+
+	private static <T, C, B extends AbstractFieldBuilder<List<C>, ?, B>> B listOption(TrackedValue<ValueList<T>> field, Text displayName, Text[] description, BiFunction<Text, ValueList<C>, B> constructor, Function<T, C> getBinder, Function<C, T> setBinder) {
+		return constructor.apply(displayName, new ValueListImpl<C>(getBinder.apply(field.getDefaultValue().getDefaultValue()), field.value().stream().map(getBinder).collect(Collectors.toList()))).setTooltip(description).setSaveConsumer(v -> {
+			ValueList<T> list = new ValueListImpl<T>(field.getDefaultValue().getDefaultValue(), v.stream().map(setBinder).collect(Collectors.toList()));
+			if (field.checkForFailingConstraints(list).isPresent()) return;
+			field.setValue(list);
+		});
 	}
 
 	private static <T, B extends AbstractFieldBuilder<T, ?, B>> B option(TrackedValue<T> field, Text displayName, Text[] description, BiFunction<Text, T, B> constructor) {
 		return option(field, displayName, description, constructor, t -> t, t -> t);
+	}
+
+	private static <T, B extends AbstractFieldBuilder<List<T>, ?, B>> B listOption(TrackedValue<ValueList<T>> field, Text displayName, Text[] description, BiFunction<Text, ValueList<T>, B> constructor) {
+		return listOption(field, displayName, description, constructor, t -> t, t -> t);
 	}
 
 	@SuppressWarnings("unchecked")
