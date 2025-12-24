6752fed4293562ff3b6feb0650df09ad5364064d
sisby-folk <sleepingdragoninn@gmail.com>
2025-12-24 16:27:28 +1100

    no star

--- src/main/java/dev/sisby/mcqoy/McQoy.java
+++ src/main/java/dev/sisby/mcqoy/McQoy.java
@@ -47,7 +47,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.*;
+import java.awt.Color;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Arrays;
